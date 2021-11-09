833715f4ba3dbba59c5d3095e26a3b350a6a762a
k3st <93670243+k3st@users.noreply.github.com>
2021-11-10 02:16:38 +0800

    Update RoiCalc.dart
    formula fix
    cleaning of UI (paddings)
    
    dark -> light mode

--- lib/screens/RoiCalc.dart
+++ lib/screens/RoiCalc.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: file_names
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:investment_calc/widgets/sidebar.dart';
+import 'package:flutter/services.dart';
 
 class RoiCalc extends StatefulWidget {
   const RoiCalc({Key? key}) : super(key: key);
@@ -11,11 +14,10 @@ class RoiCalc extends StatefulWidget {
 }
 
 class _RoiCalcState extends State<RoiCalc> {
-  @override
   TextEditingController maturity = TextEditingController();
-  TextEditingController investmentperiod = TextEditingController();
-  TextEditingController interest = TextEditingController();
+  TextEditingController initial = TextEditingController();
   TextEditingController roi = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -24,31 +26,34 @@ class _RoiCalcState extends State<RoiCalc> {
         title: const Text("Investment Calculator"),
         backgroundColor: Colors.red,
       ),
-      backgroundColor: Colors.grey[800],
+      //backgroundColor: Colors.grey[800],
       body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  child: (Text(
-                    "Basic Calculator  ",
-                    style: TextStyle(fontSize: 16, color: Colors.black54),
-                  )),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/calc');
-                  },
-                ),
-                Text(
-                  "/ Investment Computation",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.redAccent),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    child: (Text(
+                      "Basic Calculator  ",
+                      style: TextStyle(fontSize: 16, color: Colors.black54),
+                    )),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/calc');
+                    },
+                  ),
+                  Text(
+                    "Investment Computation",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.redAccent),
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 20),
             Container(
@@ -58,29 +63,61 @@ class _RoiCalcState extends State<RoiCalc> {
                 children: <Widget>[
                   Text("Return on Investment",
                       style: TextStyle(
-                        color: Colors.redAccent,
-                        fontSize: 20,
-                      )),
-                  SizedBox(height: 20),
+                          color: Colors.redAccent,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: 25),
+                  Text(
+                    "Total Amount Returned ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 10),
                   TextField(
                     textAlign: TextAlign.center,
                     controller: maturity,
                     keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
                       ),
-                      hintStyle: TextStyle(color: Colors.white38),
+                      hintStyle: TextStyle(color: Colors.black),
                       fillColor: Colors.black26,
                       filled: true,
-                      hintText: 'Amount At Maturity',
+                      hintText: 'Enter Amount',
                     ),
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.black),
                   ),
                   SizedBox(height: 20),
+                  Text(
+                    "Amount Invested ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 10),
                   TextField(
                     textAlign: TextAlign.center,
-                    controller: investmentperiod,
+                    controller: initial,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      hintStyle: TextStyle(color: Colors.black),
+                      fillColor: Colors.black26,
+                      filled: true,
+                      hintText: 'Enter Amount',
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(height: 20),
+                  /* TextField(
+                    textAlign: TextAlign.center,
+                    controller: period,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
@@ -89,27 +126,16 @@ class _RoiCalcState extends State<RoiCalc> {
                       hintStyle: TextStyle(color: Colors.white38),
                       fillColor: Colors.black26,
                       filled: true,
-                      hintText: 'Investment Period',
+                      hintText: 'Investment Period (yrs)',
                     ),
                     style: TextStyle(color: Colors.white),
-                  ),
+                  ), */
                   SizedBox(height: 20),
-                  TextField(
-                    textAlign: TextAlign.center,
-                    controller: interest,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      hintStyle: TextStyle(color: Colors.white38),
-                      fillColor: Colors.black26,
-                      filled: true,
-                      hintText: 'Interest Rate %',
-                    ),
-                    style: TextStyle(color: Colors.white),
+                  Text(
+                    "ROI % ",
+                    style: TextStyle(fontSize: 20),
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 10),
                   TextField(
                     textAlign: TextAlign.center,
                     controller: roi,
@@ -117,29 +143,26 @@ class _RoiCalcState extends State<RoiCalc> {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
                       ),
-                      hintStyle: TextStyle(color: Colors.white38),
+                      hintStyle: TextStyle(color: Colors.black),
                       fillColor: Colors.black26,
                       filled: true,
                       hintText: 'ROI',
                     ),
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.red),
+                    readOnly: true,
                   ),
                   SizedBox(
                     height: 20,
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      //Testings di ko alam formula eh nYAAHAHAH
-                      print(maturity.text);
-                      print(investmentperiod.text);
-                      print(interest.text);
-                      print(roi.text);
-                      roi.text = (int.parse(maturity.text) +
-                              int.parse(investmentperiod.text))
-                          .toString();
-                    },
-                    child: (Text('CALCULATE')),
-                  ),
+                      onPressed: () {
+                        //Returned amount / investment = roi
+                        roi.text = ((double.parse(maturity.text) /
+                                    double.parse(initial.text)) *
+                                100)
+                            .toString();
+                      },
+                      child: (Text('CALCULATE'))),
                 ],
               ),
             ),
@@ -147,5 +170,18 @@ class _RoiCalcState extends State<RoiCalc> {
         ),
       ),
     );
+  }
+
+  void calculation(
+      TextEditingController maturity, TextEditingController initial,
+      [TextEditingController? period]) {
+    int after = int.parse(maturity.text);
+    int before = int.parse(initial.text);
+    int yrs = int.parse(period!.text);
+
+    double power = pow(1, yrs) - 1;
+    double x = (after / before) + power;
+    roi.text = x.toString();
+    print(roi);
   }
 }
