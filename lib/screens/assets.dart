3d76e672d73e3600b96dcf37ba3c347a9f3f8f2f
Gab_ <gabminer05@gmail.com>
2021-11-10 21:40:26 +0800

    Added dialog box
    Not done yet no display

--- lib/screens/assets.dart
+++ lib/screens/assets.dart
@@ -3,6 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:investment_calc/widgets/sidebar.dart';
 
+int MyAssets = 1;
+List<String> assetName = <String>[];
+List<int> assetPrice = <int>[];
+
 class AssetsPage extends StatefulWidget {
   const AssetsPage({Key? key}) : super(key: key);
 
@@ -10,17 +14,18 @@ class AssetsPage extends StatefulWidget {
   _AssetsPageState createState() => _AssetsPageState();
 }
 
-class _AssetsPageState extends State<AssetsPage> {
-  int myAssets = 1;
+class _AssetsPageState extends State<AssetsPage>{
 
-  String checkAssets(assets) {
-    if (assets == 0) {
-      return 'There are no Assets to show.';
-    } else {
-      return '';
-    }
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    priceController.dispose();
+    super.dispose();
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,14 +36,64 @@ class _AssetsPageState extends State<AssetsPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              checkAssets(myAssets),
-            ),
-          ],
+          
         ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await _showMyDialog();
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
+
+  Future<void> _showMyDialog() async {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Add a Asset'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children:[
+              Text("Asset Name:",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              )),
+              TextField(
+                controller: nameController,
+              ),
+              Text("Asset Price:",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              )),
+              TextField(
+                controller: priceController,
+                
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Add'),
+            onPressed: () {
+              assetName.add(nameController.text);
+              int qty = int.parse(priceController.text);
+              assetPrice.add(qty);
+              setState((){});
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
+
+}
+
